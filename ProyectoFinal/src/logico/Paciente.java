dc32539774c731470d4b30feb86f29db9cc954e2
nsanc <nxsu0001@ce.pucmm.edu.do>
2024-11-19 15:08:17 -0400

    agregando atributos a la clase paciente.

--- ProyectoFinal/src/logico/Paciente.java
+++ ProyectoFinal/src/logico/Paciente.java
@@ -2,4 +2,13 @@ package logico;
 
 public class Paciente {
 
+	private String codPaciente;
+
+	public String getCodPaciente() {
+		return codPaciente;
+	}
+
+	public void setCodPaciente(String codPaciente) {
+		this.codPaciente = codPaciente;
+	}
 }
