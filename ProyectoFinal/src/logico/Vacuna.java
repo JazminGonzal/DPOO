d1326d3a225b93821cf3d8806fb501c6bfa6d73d
Jazmi <Jazmi@Jazmin-Laptop>
2024-11-19 15:10:40 -0400

    creando clase vacuna y sus atributos

---
+++ ProyectoFinal/src/logico/Vacuna.java
@@ -0,0 +1,16 @@
+package logico;
+
+public class Vacuna {
+
+	private String codigoVacuna;
+	
+	
+
+	public String getCodigoVacuna() {
+		return codigoVacuna;
+	}
+
+	public void setCodigoVacuna(String codigoVacuna) {
+		this.codigoVacuna = codigoVacuna;
+	}
+}
